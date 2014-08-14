8ddf540d17eb4f23979933562fa4a2b2f58763a7
Victor Nesterenko <V@vkafe.org>
2014-08-14 19:44:25 +0300

    Cleared some commented lines

--- client/map.js
+++ client/map.js
@@ -460,22 +460,6 @@ liveZones = function(map, cursor) {
   transform = function(doc) {
     var paths = google.maps.geometry.encoding.decodePath(doc.encbounds);
     return {
-      /*  google.maps.PolygonOptions:
-      clickable boolean Indicates whether this Polygon handles mouse events. Defaults to true.
-      draggable boolean If set to true, the user can drag this shape over the map. The geodesic property defines the mode of dragging. Defaults to false.
-      editable  boolean If set to true, the user can edit this shape by dragging the control points shown at the vertices and on each segment. Defaults to false.
-      fillColor string  The fill color. All CSS3 colors are supported except for extended named colors.
-      fillOpacity number  The fill opacity between 0.0 and 1.0
-      geodesic  boolean When true, edges of the polygon are interpreted as geodesic and will follow the curvature of the Earth. When false, edges of the polygon are rendered as straight lines in screen space. Note that the shape of a geodesic polygon may appear to change when dragged, as the dimensions are maintained relative to the surface of the earth. Defaults to false.
-      map Map Map on which to display Polygon.
-      paths MVCArray.<MVCArray.<LatLng>>|MVCArray.<LatLng>|Array.<Array.<LatLng|LatLngLiteral>>|Array.<LatLng|LatLngLiteral>  The ordered sequence of coordinates that designates a closed loop. Unlike polylines, a polygon may consist of one or more paths. As a result, the paths property may specify one or more arrays of LatLng coordinates. Paths are closed automatically; do not repeat the first vertex of the path as the last vertex. Simple polygons may be defined using a single array of LatLngs. More complex polygons may specify an array of arrays. Any simple arrays are converted into MVCArrays. Inserting or removing LatLngs from the MVCArray will automatically update the polygon on the map.
-      strokeColor string  The stroke color. All CSS3 colors are supported except for extended named colors.
-      strokeOpacity number  The stroke opacity between 0.0 and 1.0
-      strokePosition  StrokePosition  The stroke position. Defaults to CENTER. This property is not supported on Internet Explorer 8 and earlier.
-      strokeWeight  number  The stroke width in pixels.
-      visible boolean Whether this polygon is visible on the map. Defaults to true.
-      zIndex  number  The zIndex compared to other polys.
-      */
       fillColor: doc.color,
       fillOpacity: 0.35,
       strokeColor: doc.color,
